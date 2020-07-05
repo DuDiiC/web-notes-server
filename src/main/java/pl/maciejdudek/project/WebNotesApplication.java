6e076e1c28a849e8873fd00993ab6a80481f66a0
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-05 12:24:58 +0200

    Initial commit

---
+++ src/main/java/pl/maciejdudek/project/WebNotesApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejdudek.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/maciejdudek/project/WebNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejdudek.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
