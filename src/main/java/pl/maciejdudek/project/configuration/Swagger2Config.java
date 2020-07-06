5d7b239c8828df6eafe2c46bde3f53f2beca3337
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-06 17:55:48 +0200

    Add swagger for app

---
+++ src/main/java/pl/maciejdudek/project/configuration/Swagger2Config.java
@@ -0,0 +1,22 @@
+package pl.maciejdudek.project.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+
+    /** Turning off error basic controller in swagger2. */
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.regex("^(?!/(error).*$).*$"))
+                .build();
+    }
+}
