813bd2d23cbcc1e10fef9312105cb5c00430d196
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-07 10:47:41 +0200

    Add authentication filter and LoginCredential class
    This class read request and map JSON to Login Credentials. After that it's creating token for browser.

---
+++ src/main/java/pl/maciejdudek/project/model/DTO/LoginCredentials.java
@@ -0,0 +1,11 @@
+package pl.maciejdudek.project.model.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginCredentials {
+    private String username;
+    private String password;
+}
