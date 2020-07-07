54243ffb74c9f055ced1ad5deabc6b2059a3e84d
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-07 20:54:20 +0200

    Remove unnecessary imports and field

--- src/main/java/pl/maciejdudek/project/exceptions/UnauthorizedException.java
+++ src/main/java/pl/maciejdudek/project/exceptions/UnauthorizedException.java
@@ -1,18 +1,8 @@
 package pl.maciejdudek.project.exceptions;
 
-import java.time.LocalDateTime;
-
 public class UnauthorizedException extends RuntimeException {
-
-    private final LocalDateTime exceptionTime;
-
-    public LocalDateTime getExceptionTime() {
-        return this.exceptionTime;
-    }
 
     public UnauthorizedException() {
         super("You do not have permission to perform this operation.");
-        this.exceptionTime = LocalDateTime.now();
     }
 }
-
