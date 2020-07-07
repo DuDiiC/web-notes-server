8bc29874ba4af4115b813254cda34be2a2badb78
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-07 11:36:12 +0200

    Add properties for JWT

--- src/main/java/pl/maciejdudek/project/configuration/jtw/RestAuthenticationSuccessHandler.java
+++ src/main/java/pl/maciejdudek/project/configuration/jtw/RestAuthenticationSuccessHandler.java
@@ -1,5 +1,6 @@
 package pl.maciejdudek.project.configuration.jtw;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,16 @@ import java.io.IOException;
 
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final Long expirationTime;
+    private final String secret;
+
+    public RestAuthenticationSuccessHandler(
+            @Value("{$jwt.expirationTime}") Long expirationTime,
+            @Value("{$jwt.secret}") String secret) {
+        this.expirationTime = expirationTime;
+        this.secret = secret;
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
