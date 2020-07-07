849e2b0214f63a5e1f3b2e19be1712ad61539d4f
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-07 09:39:15 +0200

    Modify security configure
    - change rules for swagger, H2, register and API endpoints
    - add handling exception (response 401 UNAUTHORIZED) while attempting unauthorizing access

--- src/main/java/pl/maciejdudek/project/configuration/WebSecurityConfig.java
+++ src/main/java/pl/maciejdudek/project/configuration/WebSecurityConfig.java
@@ -3,11 +3,13 @@ package pl.maciejdudek.project.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import pl.maciejdudek.project.services.UserDetailsServiceImpl;
 
 @Configuration
@@ -29,14 +31,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.headers().disable();
-        http.authorizeRequests()
-                .and()
+        http
                 .authorizeRequests()
+                // swagger
+                .antMatchers("/swagger-ui.html").permitAll()
+                .antMatchers("/v2/api-docs").permitAll()
+                .antMatchers("/swagger-resources/**").permitAll()
+                // H2 DB
+                .antMatchers("/h2-console/**").permitAll()
+                // register
+                .antMatchers("/register").permitAll()
+                // API
 //                .antMatchers("/api/*").hasAnyRole("ADMIN", "USER")
                 .antMatchers("/api/*").permitAll()
-                .antMatchers("/register").permitAll()
                 .and()
-                .formLogin();
+                .formLogin().permitAll()
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
     }
 }
