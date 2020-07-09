6af849b09c735e4218d7e4418f7e3d284a5996de
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-09 14:47:18 +0200

    Add implementation for confirm registration in service

--- src/main/java/pl/maciejdudek/project/services/RegisterService.java
+++ src/main/java/pl/maciejdudek/project/services/RegisterService.java
@@ -1,8 +1,15 @@
 package pl.maciejdudek.project.services;
 
+import pl.maciejdudek.project.model.RegisterVerificationToken;
 import pl.maciejdudek.project.model.User;
 
 public interface RegisterService {
 
     User register(User user);
+
+    void createRegisterVerificationToken(User user, String token);
+
+    RegisterVerificationToken getRegisterVerificationToken(String token);
+
+    String confirmRegistration(String token);
 }
