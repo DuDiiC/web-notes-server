cfa02534777230b7b30493c6e3f7cf63029e075a
Maciej Dudek <maciejdudek96@wp.pl>
2020-07-23 18:52:12 +0200

    Add more test notes

--- src/main/java/pl/maciejdudek/project/BootstrapData.java
+++ src/main/java/pl/maciejdudek/project/BootstrapData.java
@@ -78,7 +78,7 @@ public class BootstrapData {
         note4.setNoteStatus(NoteStatus.ACTIVE);
 
         Note note5 = new Note();
-        note5.setTitle("Notatka 5 - inna");
+        note5.setTitle("Notatka 5 - test markdowna");
         note5.setContent("## Treść innej notatki  \n- test\n- działania składki\n- języka markdown");
         note5.setUser(maciej);
         note5.setCreatedAt(LocalDateTime.now());
@@ -86,17 +86,38 @@ public class BootstrapData {
 
         Note note6 = new Note();
         note6.setTitle("Notatka 6");
-        note6.setContent("Treść notatki 6.");
-        note6.setUser(dawid);
+        note6.setContent("Zobaczymy jak wygląda kod:\n\twcięcie");
+        note6.setUser(maciej);
         note6.setCreatedAt(LocalDateTime.now());
         note6.setNoteStatus(NoteStatus.ACTIVE);
 
         Note note7 = new Note();
         note7.setTitle("Notatka 7");
-        note7.setContent("Treść notatki 7.");
-        note7.setUser(marcin);
+        note7.setContent("Albo czy działa tabela:\n| | |\n|:---:|:---:|\n| pierwsza kolumna | drugia kolumna |\n_pogrubienie_ i *pogrubienie*");
+        note7.setUser(maciej);
         note7.setCreatedAt(LocalDateTime.now());
         note7.setNoteStatus(NoteStatus.ACTIVE);
+
+        Note note8 = new Note();
+        note8.setTitle("Notatka 8");
+        note8.setContent("Kod wygląda tak:\n```\n\tpublic static void main(String[] argv) {\n\t\tNote note = new Note();\n\t}\n```");
+        note8.setUser(maciej);
+        note8.setCreatedAt(LocalDateTime.now());
+        note8.setNoteStatus(NoteStatus.ACTIVE);
+
+        Note note9 = new Note();
+        note9.setTitle("Notatka 9 - zarchiwizowana.");
+        note9.setContent("## Treść usuniętej notatki.\n*pogrubiony tekst*");
+        note9.setUser(maciej);
+        note9.setCreatedAt(LocalDateTime.now());
+        note9.setNoteStatus(NoteStatus.ARCHIVED);
+
+        Note note10 = new Note();
+        note10.setTitle("Notatka 10 - usunięta");
+        note10.setContent("[ta notatka jest usunięta](https://google.pl)");
+        note10.setUser(maciej);
+        note10.setCreatedAt(LocalDateTime.now());
+        note10.setNoteStatus(NoteStatus.DELETED);
 
         noteRepository.save(note1);
         noteRepository.save(note2);
@@ -105,5 +126,8 @@ public class BootstrapData {
         noteRepository.save(note5);
         noteRepository.save(note6);
         noteRepository.save(note7);
+        noteRepository.save(note8);
+        noteRepository.save(note9);
+        noteRepository.save(note10);
     }
 }
