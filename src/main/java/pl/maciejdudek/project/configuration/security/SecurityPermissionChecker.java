cc94cd6155b29122264549041b699c40a62df7b9
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-01-30 10:39:47 +0100

    Autowired constructor -> Lombok annotation

--- src/main/java/pl/maciejdudek/project/configuration/security/SecurityPermissionChecker.java
+++ src/main/java/pl/maciejdudek/project/configuration/security/SecurityPermissionChecker.java
@@ -1,20 +1,17 @@
 package pl.maciejdudek.project.configuration.security;
 
+import lombok.RequiredArgsConstructor;
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.stereotype.Component;
 import pl.maciejdudek.project.repositories.NoteRepository;
 import pl.maciejdudek.project.repositories.UserRepository;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityPermissionChecker {
 
     private final UserRepository userRepository;
     private final NoteRepository noteRepository;
-
-    public SecurityPermissionChecker(UserRepository userRepository, NoteRepository noteRepository) {
-        this.userRepository = userRepository;
-        this.noteRepository = noteRepository;
-    }
 
     public boolean userIsAdmin(String username) {
         return userRepository.findByUsername(username).orElseThrow(
