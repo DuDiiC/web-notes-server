76a20017ba064ca372cb2709256b8fe6492ba88a
Maciej Dudek <Maciej.Dudek.DEV@gmail.com>
2021-02-04 16:31:50 +0100

    Add tests for security

--- src/main/java/pl/maciejdudek/project/model/DTO/LoginDTO.java
+++ src/main/java/pl/maciejdudek/project/model/DTO/LoginDTO.java
@@ -1,10 +1,14 @@
 package pl.maciejdudek.project.model.DTO;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginDTO {
     @ApiModelProperty(notes = "username")
     private String username;
